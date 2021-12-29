6758f9d3c15f42904478c274da05d4a04b7a10da
Elyseeez <60116004+Elyseeez@users.noreply.github.com>
2021-12-29 16:50:42 +0200

    up

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import CartItem from './cart-item';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {clearCart, removeCartItem} from '../redux/actions/cart';
 
 
 function Cart () {
+    const  dispatch = useDispatch();
     const {items, totalPrice, totalCount} = useSelector (({cart}) => cart);
-    console.log(items);
+
+    const onRemoveItem = (id) => { 
+        if(window.confirm('really?')) { 
+            dispatch(removeCartItem(id));
+        }
+    };
 
     const addedSushi = Object.keys(items).map((key) =>{
         return items[key].items[0];
     });
-    console.log(1, addedSushi);
+    const onClearCart = () => {
+        dispatch(clearCart());
+    }
 
     return( 
         <div className="cart">
             <div className="cart__items-list">
                 {addedSushi.map((obj) => (
-                    <CartItem name={obj.name} price={obj.price} />
+                    <CartItem 
+                    id={obj.id}
+                    name={obj.name} 
+                    price={obj.price} 
+                    totalPrice={items[obj.id].totalPrice} 
+                    totalCount={items[obj.id].items.length}
+                    onRemove={onRemoveItem} />
                 ))}
             
             </div>
@@ -24,6 +39,9 @@ function Cart () {
               <div className="cart__bottom-details">
                 <span> Total Ordered <b>{totalCount}</b> </span>
                 <span> total price: <b>{totalPrice} $</b> </span>
+              </div>
+              <div className="cart__clear"> 
+              <span onClick={onClearCart}>Clear basket</span>
               </div>
               </div>
         </div>
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,7 +3,7 @@ import MenuItem from "./sushi-block/menu-item";
 import LoadingBlock from "./sushi-block/loading-block";
 import {useSelector, useDispatch,} from 'react-redux';
 import {fetchSushi} from '../redux/actions/sushi';
-import {addSushiToCart} from '../redux/actions/cart';
+// import {addSushiToCart} from '../redux/actions/cart';
 
 function Menu () {
   
@@ -11,6 +11,7 @@ function Menu () {
  
   const items = useSelector(({sushi}) => sushi.items);
   const isLoaded = useSelector(({sushi}) => sushi.isLoaded);
+  const cartItems = useSelector(({ cart }) => cart.items);
 
   React.useEffect (() =>  { 
       if (!items.lenght) { 
@@ -32,7 +33,7 @@ const handleAddSushiToCart= obj => {
             {isLoaded ?
              items.map((obj) =>  <MenuItem onClickAddSushi={handleAddSushiToCart} 
                 key={obj.id}
-                isLoading={true} 
+                addedCount={cartItems[obj.id] && cartItems[obj.id].items.length}
                 {...obj}
                 />)
                 : Array(16).fill(0)
